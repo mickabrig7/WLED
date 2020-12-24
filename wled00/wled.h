8eb82227ba24f1ac54a7ac5d8dd8938bc46b47ba
mickabrig7 <mickaelgarabello7@gmail.com>
2020-12-25 00:46:34 +0100

    Configure hardware

--- wled00/wled.h
+++ wled00/wled.h
@@ -11,7 +11,7 @@
 #define VERSION 2012210
 
 //uncomment this if you have a "my_config.h" file you'd like to use
-//#define WLED_USE_MY_CONFIG
+#define WLED_USE_MY_CONFIG
 
 // ESP8266-01 (blue) got too little storage space to work with WLED. 0.10.2 is the last release supporting this unit.
 
